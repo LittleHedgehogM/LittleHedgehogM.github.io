552eecb4877ca6e297d1a2467f9be61745cf770c
LittleHedgehogM <1041495644good@gmail.com>
2025-03-05 16:06:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
   ],
-  base: '/', 
+  base: './', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
