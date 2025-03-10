a554430342474ad24b489200a604abf568875442
LittleHedgehogM <1041495644good@gmail.com>
2025-03-10 17:28:57 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-console.log('Vue app is mounting...');
+console.log('Vue app is mounting...')
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
